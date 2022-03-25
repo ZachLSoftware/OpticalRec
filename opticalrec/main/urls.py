dbf9620b7286764bf1569330f719dc6680c87f02
Zach Larsen <zacharyjlarsen@gmail.com>
2022-03-25 16:41:37 +0000

    Changed exported data to friendly names, not id's

--- opticalrec/main/urls.py
+++ opticalrec/main/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('extract_all_data/<int:vid_id>', views.extractAllData, name= 'extract_all_data'),
     path('extract_data/<int:label_id>', views.extractData, name= 'extract_data'),
     path('toggleTheme/<str:theme>', views.toggleTheme, name = "toggleTheme"),
+    path('export', views.exportToCSV, name='export_users_csv')
 
 ]
